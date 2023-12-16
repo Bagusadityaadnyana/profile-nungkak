badd84b9a5df1fe4e7743eb45d012b8149d687c1
Bagusadityaadnyana <Bagusadityaadnyana@gmail.com>
2023-12-16 23:29:30 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/index.html'],
+  theme: {
+    extend: {
+      letterSpacing: {
+        nyempit: '-0.5em',
+        sepasi: '.5em',
+      },
+    },
+  },
+  plugins: [],
+};
